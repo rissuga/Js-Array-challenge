2a7166602f433c03d32f033b14cb9d5a7afdab5f
risa <risairma21@gmail.com>
2023-03-17 19:10:14 +0700

    feat: add filter car by availability

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -1,4 +1,5 @@
 function filterCarByAvailability(cars) {
+
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
@@ -6,7 +7,13 @@ function filterCarByAvailability(cars) {
   const result = [];
 
   // Tulis code-mu disini
+  for( let i=0; i < cars.length; i++) {
+    if( cars[i].available === true) {
+       result.push(cars[i]);
+    }
+  }
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
 }
+
